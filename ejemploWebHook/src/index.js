06f725d89138054a492ae6394c789c358838128c
EMIGonH123 <metalica.el.01@gmail.com>
2019-11-01 17:37:19 -0600

    API funcionando

--- ejemploWebHook/src/index.js
+++ ejemploWebHook/src/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
+const request = require('request');
 
 const app = express();
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+//const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
+const PAGE_ACCESS_TOKEN= "EAAHeZAwIZASdgBACnwL9zpAu6wUMbsHPNprFGCZAnHsRySqdKXBdFyLfnWzQefZCaV4fNKZCHlWZBEdfurDJ7ug1jDuBy28XZCdY2AqTn7v8MXtvDxbGLKOt4qrTMOkzxHTv06fYkwJgeGqlVsYAm9pT7qyZCk9Q3zb8oHYuPVJXHhuqXZAvngJVo";
+console.log("---> PAGE_ACCESS_TOKEN: "+PAGE_ACCESS_TOKEN);
 app.set("port", process.env.PORT || 1337);
 app.set("view engine", "jade");
 
@@ -13,17 +16,96 @@ app.use(express.json());
 //Funciones para el desarrollo del BOT
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
+  console.log("---> Entrando al handleMessage");
+  let response;
 
+  // Check if the message contains text
+  if (received_message.text) {
+
+    // Create the payload for a basic text message
+    response = {
+      "text": `Enviaste el siguiente mensaje: "${received_message.text}".`
+    }
+  }else if (received_message.attachments) {
+  
+    // Gets the URL of the message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [{
+            "title": "¿Es tu imagen?",
+            "subtitle": "Selecciona una opción",
+            "image_url": attachment_url,
+            "buttons": [
+              {
+                "type": "postback",
+                "title": "Simon",
+                "payload": "yes",
+              },
+              {
+                "type": "postback",
+                "title": "Nel",
+                "payload": "no",
+              }
+            ],
+          }]
+        }
+      }
+    }
+  } 
+
+  // Sends the response message
+  callSendAPI(sender_psid, response);
+
+  console.log("---> Saliendo del handleMessage");
 }
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
+  let response;
+  
+  // Get the payload for the postback
+  let payload = received_postback.payload;
 
+  // Set the response based on the postback payload
+  if (payload === 'yes') {
+    response = { "text": "Va que va, gracias!!" }
+  } else if (payload === 'no') {
+    response = { "text": "Lo lamento, perdí tu imagen :(" }
+  }
+  // Send the message to acknowledge the postback
+  callSendAPI(sender_psid, response);
 }
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
+  console.log("---> Entrando al callSendAPI");
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
 
+  // Send the HTTP request to the Messenger Platform
+  console.log("---> PAGE_ACCESS_TOKEN: "+PAGE_ACCESS_TOKEN);
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  });
+  console.log("---> Saliendo del callSendAPI"); 
 }
 
 
@@ -79,6 +161,15 @@ app.post('/webhook', (req, res) => {
       // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
       console.log('Sender PSID: ' + sender_psid);
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
+
     });
 
     // Returns a '200 OK' response to all requests
